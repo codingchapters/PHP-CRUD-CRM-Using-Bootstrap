f09fa0f54af182f631764583d11c1c9dde3d719f
codingchapters <codingchapters@gmail.com>
2015-08-05 12:52:59 +0530

    Create header.php

---
+++ layout/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Simple CRUD CRM Using Bootstrap | Coding Chapters</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.4 -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+	 <!-- FontAwesome 4.3.0 -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container">
+	<div class="row">
+	<div class="col-md-12 col-sm-12">
+		<div class="header">
+		<h1>Simple CRUD CRM Using Bootstrap</h1>
+		</div>
+		<div class="content">
